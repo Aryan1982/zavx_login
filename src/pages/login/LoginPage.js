a834e8e705e018d1015cec4c318b263bda570f15
Aryan1982 <ap1982o3@gmail.com>
2024-03-17 21:48:50 +0530

    bugs fixed

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -7,67 +7,66 @@ const LoginPage = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const baseURL = 'https://server.zavx.in';
-  // const baseURL = 'http://localhost:8000'; 
+  const baseURL = 'http://server.zavx.in';
+  // const baseURL = 'http://localhost:4000';
 
   const handleLogin = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const client_id = urlParams.get('client_id');
     const redirect_uri = urlParams.get('redirect_uri');
     const state = urlParams.get('state');
-  
+
     axios.post(`${baseURL}/user/alexalogin/`, { email, password }, {
       params: { client_id, redirect_uri, state },
       validateStatus: function (status) {
         return status >= 200 && status < 300; // Resolve promise for 2xx status codes
       }
     })
-    .then(response => {
-        if (response.status === 200) {
+      .then(response => {
+        if (response) {
           alert('Login successful!');
-          window.location.href = response.data.redirect;
+          window.location.href = response.data.data.redirect;
         } else {
-      }
-    })
-    .catch(error => {
-      // Handle errors
-      setError('Invalid username or password');
-    });
+        }
+      })
+      .catch(error => {
+        setError('Invalid username or password');
+      });
   };
-  
+
 
   return (
     <div className="container">
-    <div className="overlay"></div>
-        <div className="form-container">
+      <div className="overlay"></div>
+      <div className="form-container">
         <h1>Log In</h1>
         <div className="form-group">
-            {/* <label>Username:</label> */}
-            <input
+          {/* <label>Username:</label> */}
+          <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder='Enter Email'
-            />
+          />
         </div>
         <div className="form-group">
-            {/* <label>Password:</label> */}
-            <input
+          {/* <label>Password:</label> */}
+          <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder='Enter Password'
-            />
+          />
         </div>
         {error && <div className="error-message">{error}</div>}
         <button className="btn" onClick={handleLogin}>Login</button>
         <p className='note'>( Note: Login with your account registered with Zavx )</p>
         <div className='footer'>
 
-            <h1 className='logo'>ZAVX</h1>
-            <h2 className='tagline'>#aestheticizing technology</h2>
+          <h1 className='logo'>ZAVX</h1>
+          <h2 className='tagline'>#aestheticizing technology</h2>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
